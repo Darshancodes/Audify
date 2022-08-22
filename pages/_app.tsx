d3ad3d568432cca05c00690f5a4ff5682bd1f469
aman-janwani <amanjanwani1486@gmail.com>
2022-08-22 21:48:56 +0530

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,45 @@
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
 
+import 'react-toastify/dist/ReactToastify.css';
+import { AudioPlayerProvider } from '../src/provider/audio-player';
+// import PageLayout from '../src/layouts/page-layout';
+import { ThemeProvider } from 'next-themes';
+import { AppWeb3Provider } from '../src/provider/app-web3';
+import { ThirdwebProvider } from '@3rdweb/react';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { RinkeByChainID } from '../src/constants';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return (
+    <ThirdwebProvider  
+      supportedChainIds={RinkeByChainID}
+      connectors={{
+        injected: {},
+      }}
+    >
+      <AppWeb3Provider>
+        <AudioPlayerProvider>
+           <ThemeProvider attribute="class" defaultTheme="light">
+            {getLayout(<Component {...pageProps} />)}
+            <ToastContainer />
+          </ThemeProvider>
+        </AudioPlayerProvider>
+      </AppWeb3Provider>
+    </ThirdwebProvider>
+  );
+}
 
 export default MyApp;
